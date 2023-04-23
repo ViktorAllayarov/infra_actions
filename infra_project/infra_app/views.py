7130e02eee20fdc0c5cbdf888b00c74b49e064c0
Paradox <viktorall25@yandex.ru>
2023-04-23 13:32:54 +0300

    check push

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("У меня получилось! ага")
+    return HttpResponse("У меня получилось! аг")
 
 
 def second_page(request):
